8f8dd1af29cfda6603ea2f07355a2efa0a843c0d
RadyaBen <lizoguben.radya@gmail.com>
2022-12-15 15:31:23 +0200

    Add a service to store values in the user's home directory

--- weather.js
+++ weather.js
@@ -1,12 +1,26 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+	try {
+		await saveKeyValue('token', token);
+		printSuccess('Token saved');
+	} catch (e) {
+		printError(e.message);
+	}
+};
 
 const initCLI = () => {
 	const args = getArgs(process.argv);
 
 	if (args.h) {
 		printHelp();
+	}
+
+	if (args.t) {
+		return saveToken(args.t);
 	}
 };
 
