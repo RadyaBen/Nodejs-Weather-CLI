61c4979d89de8ca4030d195ce073f4832048e641
RadyaBen <lizoguben.radya@gmail.com>
2022-12-16 16:21:02 +0200

    Add a weather service

--- weather.js
+++ weather.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { getWeather } from './services/api.service.js';
 import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
 	if (!token.length) {
@@ -10,10 +11,25 @@ const saveToken = async (token) => {
 	}
 
 	try {
-		await saveKeyValue('token', token);
-		printSuccess('Token saved');
+		await saveKeyValue(TOKEN_DICTIONARY.token, token);
+		printSuccess('The token has been saved');
 	} catch (e) {
 		printError(e.message);
+	}
+};
+
+const getForcast = async () => {
+	try {
+		const weather = await getWeather(process.env.CITY);
+		console.log(weather);
+	} catch (e) {
+		if (e?.response?.status === 404) {
+			printError('The city was entered incorrectly');
+		} else if (e?.response?.status === 401) {
+			printError('The token was entered incorrectly')
+		} else {
+			printError(e.message);
+		}
 	}
 };
 
@@ -27,6 +43,8 @@ const initCLI = () => {
 	if (args.t) {
 		return saveToken(args.t);
 	}
+
+	getForcast();
 };
 
 initCLI();
